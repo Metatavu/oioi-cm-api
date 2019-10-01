240350f633209c68bb0ea03ae0df44aa384395a8
Antti Leppä <antti.leppa@metatavu.fi>
2019-10-01 06:25:00 +0300

    Initial commit

---
+++ src/main/java/fi/metatavu/oioi/cm/rest/JaxRsActivator.java
@@ -0,0 +1,9 @@
+package fi.metatavu.oioi.cm.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/v1")
+public class JaxRsActivator extends Application {
+  
+}
---
+++ src/main/java/fi/metatavu/oioi/cm/rest/SystemRESTService.java
@@ -0,0 +1,36 @@
+package fi.metatavu.oioi.cm.rest;
+
+import javax.enterprise.context.RequestScoped;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * System REST Services
+ * 
+ * @author Antti Leppä
+ */
+@Path ("/system")
+@RequestScoped
+@Produces (MediaType.APPLICATION_JSON)
+@Consumes (MediaType.APPLICATION_JSON)
+public class SystemRESTService {
+  
+  /**
+   * Returns pong
+   * 
+   * @return pong in plain text
+   */
+  @GET
+  @Path ("/ping")
+  @Produces (MediaType.TEXT_PLAIN)
+  public Response getPing(@Context HttpServletRequest request) {
+    return Response.ok("pong").build();
+  }
+  
+}
