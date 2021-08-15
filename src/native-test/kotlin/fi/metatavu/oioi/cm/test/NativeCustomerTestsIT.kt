17af9b78832f31110bdd287a7b4d71766de21e3e
Antti Leppä <antti.leppa@metatavu.fi>
2021-08-15 11:58:29 +0300

    Added test resources to native tests

--- src/native-test/kotlin/fi/metatavu/oioi/cm/test/NativeCustomerTestsIT.kt
+++ src/native-test/kotlin/fi/metatavu/oioi/cm/test/NativeCustomerTestsIT.kt
@@ -1,6 +1,9 @@
 package fi.metatavu.oioi.cm.test
 
+import fi.metatavu.ikioma.integrations.test.functional.resources.MysqlResource
+import fi.metatavu.oioi.cm.test.functional.resources.KeycloakTestResource
 import fi.metatavu.oioi.cm.test.functional.tests.CustomerTestsIT
+import io.quarkus.test.common.QuarkusTestResource
 import io.quarkus.test.junit.NativeImageTest
 
 /**
@@ -9,6 +12,10 @@ import io.quarkus.test.junit.NativeImageTest
  * @author Antti Leppä
  */
 @NativeImageTest
+@QuarkusTestResource.List(
+    QuarkusTestResource(KeycloakTestResource::class),
+    QuarkusTestResource(MysqlResource::class)
+)
 class NativeCustomerTestsIT : CustomerTestsIT() {
 
 }
