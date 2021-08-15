f8e7c89efa57941ea657fa3ee143f36b2e553494
Antti Leppä <antti.leppa@metatavu.fi>
2021-08-15 14:28:16 +0300

    Added native tests for resources and wall functinalities

---
+++ src/native-test/kotlin/fi/metatavu/oioi/cm/test/NativeResourceTestsIT.kt
@@ -0,0 +1,21 @@
+package fi.metatavu.oioi.cm.test
+
+import fi.metatavu.ikioma.integrations.test.functional.resources.MysqlResource
+import fi.metatavu.oioi.cm.test.functional.resources.KeycloakTestResource
+import fi.metatavu.oioi.cm.test.functional.tests.ResourceTestsIT
+import io.quarkus.test.common.QuarkusTestResource
+import io.quarkus.test.junit.NativeImageTest
+
+/**
+ * Native functional tests for resources
+ *
+ * @author Antti Leppä
+ */
+@NativeImageTest
+@QuarkusTestResource.List(
+    QuarkusTestResource(KeycloakTestResource::class),
+    QuarkusTestResource(MysqlResource::class)
+)
+class NativeResourceTestsIT : ResourceTestsIT() {
+
+}
